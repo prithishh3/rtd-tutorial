0a3b782a3d87fcc1db97ee654670ea242fb2a0b5
Prithish Halder <32960075+prithishh3@users.noreply.github.com>
2022-07-24 16:24:25 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,64 @@ html_static_path = ['_static']
 def setup(app):
     app.add_css_file('style1.css')
     
+# Otherwise VTK reader issues on some systems, causing sphinx to crash. See also #226.
+locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
+
+if sys.version_info >= (3, 0):
+    import faulthandler
+
+    faulthandler.enable()
+
+sys.path.insert(0, os.path.abspath("."))
+import make_external_gallery
+import make_tables
+
+make_external_gallery.make_example_gallery()
+make_tables.make_all_tables()
+
+# -- pyvista configuration ---------------------------------------------------
+import pyvista
+
+# Manage errors
+pyvista.set_error_output_file("errors.txt")
+# Ensure that offscreen rendering is used for docs generation
+pyvista.OFF_SCREEN = True  # Not necessary - simply an insurance policy
+# Preferred plotting style for documentation
+pyvista.set_plot_theme("document")
+pyvista.global_theme.window_size = [1024, 768]
+pyvista.global_theme.font.size = 22
+pyvista.global_theme.font.label_size = 22
+pyvista.global_theme.font.title_size = 22
+pyvista.global_theme.return_cpos = False
+pyvista.set_jupyter_backend(None)
+# Save figures in specified directory
+pyvista.FIGURE_PATH = os.path.join(os.path.abspath("./images/"), "auto-generated/")
+if not os.path.exists(pyvista.FIGURE_PATH):
+    os.makedirs(pyvista.FIGURE_PATH)
+
+# necessary when building the sphinx gallery
+pyvista.BUILDING_GALLERY = True
+os.environ['PYVISTA_BUILDING_GALLERY'] = 'true'
+
+# SG warnings
+import warnings
+
+warnings.filterwarnings(
+    "ignore",
+    category=UserWarning,
+    message="Matplotlib is currently using agg, which is a non-GUI backend, so cannot show the figure.",
+)
+
+
+# -- General configuration ------------------------------------------------
+numfig = False
+html_logo = "./_static/pyvista_logo_sm.png"
+
+sys.path.append(os.path.abspath("./_ext"))
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 
 
 extensions = [
