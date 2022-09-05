70f47a14a99d1818fd8694d8b20669f765aa9e13
Prithish Halder <32960075+prithishh3@users.noreply.github.com>
2022-09-05 23:46:05 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,11 +64,6 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
-# Build PDF & ePub
-formats:
-  - epub
-  - pdf
-
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
